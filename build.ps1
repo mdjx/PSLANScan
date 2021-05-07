46702e70ae8b22f3c60710fcb2463ca4c8e2b9ed
md <md@syslab.com.au>
2021-05-07 23:20:26 +0930

    installed modules to CU scope

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@ param(
     [string[]]$Task = 'default'
 )
 
-if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer }
-if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester }
-if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake }
-if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy }
+if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Scope CurrentUser}
+if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser}
+if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser}
+if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser}
 
 Invoke-psake -buildFile "$PSScriptRoot\psakeBuild.ps1" -taskList $Task -Verbose:$VerbosePreference
